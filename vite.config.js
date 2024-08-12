5c86e437a352e702f2e24bfd50c7ddbdb3db491e
ItsTarikBTW <tarikturbo007@gmail.com>
2024-08-13 00:47:16 +0100

    force https 3

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    base: 'https://quran-academy.itstarik.me/',
     plugins: [
         laravel({
             input: 'resources/js/app.tsx',
