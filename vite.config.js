9c30d845127034fbe86ae078214e949970311151
ItsTarikBTW <tarikturbo007@gmail.com>
2024-08-13 00:19:07 +0100

    fix: force https

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    base: process.env.APP_URL,
     plugins: [
         laravel({
             input: 'resources/js/app.tsx',
