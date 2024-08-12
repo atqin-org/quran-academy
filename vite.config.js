c7ea5e53a4b7a6f958c5e3fca20d42e124fc1a47
ItsTarikBTW <tarikturbo007@gmail.com>
2024-08-13 00:40:15 +0100

    fix: atmpt2 force https

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: process.env.APP_URL,
     plugins: [
         laravel({
             input: 'resources/js/app.tsx',
