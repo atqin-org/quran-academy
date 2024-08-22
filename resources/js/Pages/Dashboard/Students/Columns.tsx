ec023f8ac1d6002f19a28717b4e9f5195e37d159
ItsTarikBTW <tarikturbo007@gmail.com>
2024-08-22 20:28:17 +0100

    feat: sample table with static data

---
+++ resources/js/Pages/Dashboard/Students/Columns.tsx
@@ -0,0 +1,232 @@
+import { ColumnDef } from "@tanstack/react-table";
+import { MoreHorizontal, ArrowUpDown } from "lucide-react";
+import { Checkbox } from "@/Components/ui/checkbox";
+import { Button } from "@/Components/ui/button";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/Components/ui/dropdown-menu";
+import { Link } from "@inertiajs/react";
+
+export type StudentDisplay = {
+    id: string;
+    name: string;
+    age: number;
+    club: string;
+    ahzab: number;
+    category: string;
+    gender: "male" | "female";
+    birthdate: Date;
+    subscription: number;
+    insurance_expire_at: Date | null;
+    subscription_expire_at: Date | null;
+};
+
+export const columns: ColumnDef<StudentDisplay>[] = [
+    {
+        id: "select",
+        header: ({ table }) => (
+            <Checkbox
+                checked={
+                    table.getIsAllPageRowsSelected() ||
+                    (table.getIsSomePageRowsSelected() && "indeterminate")
+                }
+                onCheckedChange={(value) =>
+                    table.toggleAllPageRowsSelected(!!value)
+                }
+                aria-label="Select all"
+            />
+        ),
+        cell: ({ row }) => (
+            <div className="mx-auto flex items-center justify-end">
+                <Checkbox
+                    checked={row.getIsSelected()}
+                    onCheckedChange={(value) => row.toggleSelected(!!value)}
+                    aria-label="Select row"
+                />
+            </div>
+        ),
+        enableSorting: false,
+        enableHiding: false,
+    },
+    {
+        id: "الاسم",
+        accessorKey: "name",
+        header: () => <div className="text-start">الاسم الكامل</div>,
+        cell: ({ row }) => {
+            const name = row.getValue("الاسم") as string;
+            return (
+                <Link
+                    href={`/dashboard/students/${row.original.id}`}
+                    className="text-start font-medium hover:underline hover:underline-offset-2"
+                >
+                    {name}
+                </Link>
+            );
+        },
+    },
+    {
+        id: "العمر",
+        accessorKey: "age",
+        header: ({ column }) => {
+            return (
+                <div className="flex justify-center">
+                    <Button
+                        variant="ghost"
+                        onClick={() =>
+                            column.toggleSorting(column.getIsSorted() === "asc")
+                        }
+                        className="flex justify-start items-center gap-1"
+                    >
+                        العمر
+                        <ArrowUpDown className="ml-2 h-4 w-4" />
+                    </Button>
+                </div>
+            );
+        },
+        cell: ({ row }) => {
+            const age = row.getValue("العمر") as number;
+            return <div className="text-center font-medium">{age}</div>;
+        },
+    },
+    {
+        id: "الحزب",
+        accessorKey: "ahzab",
+        header: ({ column }) => {
+            return (
+                <div className="flex justify-center">
+                    <Button
+                        variant="ghost"
+                        onClick={() =>
+                            column.toggleSorting(column.getIsSorted() === "asc")
+                        }
+                        className="flex items-center gap-1"
+                    >
+                        الاحزاب
+                        <ArrowUpDown className="ml-2 h-4 w-4" />
+                    </Button>
+                </div>
+            );
+        },
+        cell: ({ row }) => {
+            const ahzab = row.getValue("الحزب") as number;
+            return <div className="text-center font-medium">{ahzab}</div>;
+        },
+    },
+    {
+        id: "النادي",
+        accessorKey: "club",
+        header: () => <div className="text-start">النادي</div>,
+    },
+    {
+        id: "الفئة",
+        accessorKey: "category",
+        header: () => <div className="text-start">الفئة</div>,
+    },
+    {
+        id: "التامين",
+        accessorKey: "insurance_expire_at",
+        header: () => <div className="text-center">التامين</div>,
+        cell: ({ row }) => {
+            const insurance = row.getValue("التامين") as Date | null;
+            const isDatePassed = insurance ? new Date() > insurance : false;
+            return (
+                <div className="flex justify-center font-medium">
+                    <div
+                        className={`w-fit p-2 rounded-xl ${
+                            isDatePassed
+                                ? "bg-red-200 text-red-700"
+                                : "bg-emerald-200 text-emerald-700"
+                        }`}
+                    >
+                        {insurance
+                            ? new Intl.DateTimeFormat("ar-DZ", {
+                                  month: "2-digit",
+                                  day: "2-digit",
+                              }).format(insurance)
+                            : "N/A"}
+                    </div>
+                </div>
+            );
+        },
+    },
+    {
+        id: "الاشتراك",
+        accessorKey: "subscription_expire_at",
+        header: () => <div className="text-center">الاشتراك</div>,
+        cell: ({ row }) => {
+            const subscription = row.getValue("الاشتراك") as Date | null;
+            const isDatePassed = subscription
+                ? new Date() > subscription
+                : false;
+            return (
+                <div className="flex justify-center font-medium">
+                    <div
+                        className={`w-fit p-2 rounded-xl ${
+                            isDatePassed
+                                ? "bg-red-200 text-red-700"
+                                : "bg-emerald-200 text-emerald-700"
+                        }`}
+                    >
+                        {subscription
+                            ? new Intl.DateTimeFormat("ar-DZ", {
+                                  month: "2-digit",
+                                  day: "2-digit",
+                              }).format(subscription)
+                            : "N/A"}
+                    </div>
+                </div>
+            );
+        },
+    },
+    {
+        id: "مبلغ الاشتراك",
+        accessorKey: "subscription",
+        header: () => <div className="text-start">الاشتراك الشهري</div>,
+        cell: ({ row }) => {
+            const subscription = parseFloat(row.getValue("مبلغ الاشتراك"));
+            const formatted = new Intl.NumberFormat("ar-DZ", {
+                style: "currency",
+                currency: "DZD",
+            }).format(subscription);
+
+            return <div className="text-start font-medium">{formatted}</div>;
+        },
+    },
+    {
+        id: "المزيد",
+        header: () => <div className="text-start">المزيد</div>,
+        cell: ({ row }) => {
+            const payment = row.original;
+            return (
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="ghost" className="h-8 w-8 p-0">
+                            <span className="sr-only">Open menu</span>
+                            <MoreHorizontal className="h-4 w-4" />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>المزيد</DropdownMenuLabel>
+                        <DropdownMenuItem
+                            onClick={() =>
+                                navigator.clipboard.writeText(payment.id)
+                            }
+                        >
+                            Copy payment ID
+                        </DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>View customer</DropdownMenuItem>
+                        <DropdownMenuItem>
+                            View payment details
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            );
+        },
+    },
+];
--- resources/js/Pages/Dashboard/Students/Index.tsx
+++ resources/js/Pages/Dashboard/Students/Index.tsx
@@ -1,6 +1,50 @@
 import { Head, Link } from "@inertiajs/react";
 import { PageProps } from "@/types";
 import DashboardLayout from "@/Layouts/DashboardLayout";
+import { StudentDisplay, columns } from "./Columns";
+import { DataTable } from "./DataTable";
+
+const data: StudentDisplay[] = [
+    {
+        id: "1",
+        name: "احمد علي",
+        age: 12,
+        club: "مالك ابن أنس",
+        ahzab: 1,
+        category: "التلقين",
+        gender: "male",
+        birthdate: new Date(),
+        subscription: 2000,
+        insurance_expire_at: null,
+        subscription_expire_at:  new Date(),
+    },
+    {
+        id: "2",
+        name: "طارق حمومي",
+        age: 19,
+        club: "مالك ابن أنس",
+        ahzab: 30,
+        category: "التلقين",
+        gender: "female",
+        birthdate: new Date(),
+        subscription: 2000,
+        insurance_expire_at: new Date(),
+        subscription_expire_at:null,
+    },
+    {
+        id: "3",
+        name: "عمر عباس",
+        age: 24,
+        club: "مالك ابن أنس",
+        ahzab: 18,
+        category: "التلقين",
+        gender: "male",
+        birthdate: new Date(),
+        subscription: 2000,
+        insurance_expire_at: new Date("2023-12-12"),
+        subscription_expire_at: new Date("2024-12-25"),
+    },
+];
 interface DashboardProps extends PageProps {
     students: any[];
 }
@@ -8,13 +52,19 @@ export default function Dashboard({ auth, students }: DashboardProps) {
     return (
         <DashboardLayout user={auth.user}>
             <Head title="⚠️ Under Construction" />
-            <div className="flex flex-col items-center justify-center  h-full">
-                <Link
-                    className="text-xl text-primary-foreground font-bold text-center bg-primary p-4 rounded-xl select-none"
-                    href="/dashboard/students/create"
-                >
-                    سجل طالب جديد
-                </Link>
+            <div className="flex flex-col items-center justify-start h-full">
+                <div className="flex items-center w-full">
+                    <h1 className="text-4xl font-bold text-gray-900">الطالب</h1>
+                    <div className="flex-1"></div>
+                    <Link
+                        className="text-primary-foreground font-bold text-center bg-primary p-4 rounded-xl select-none"
+                        href="/dashboard/students/create"
+                    >
+                        سجل طالب جديد
+                    </Link>
+                </div>
+                <DataTable columns={columns} data={data} />
+
                 <div className="mt-4">
                     <h1 className="text-2xl font-bold text-center">الطلاب</h1>
                     <div className="grid grid-cols-1 gap-2 mt-4">
