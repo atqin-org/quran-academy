4390ee6f538245219ee4b6e58b7c006c7ac5ca6f
ItsTarikBTW <tarikturbo007@gmail.com>
2025-11-30 11:51:12 +0100

    Refactor code structure for improved readability and maintainability

--- database/factories/StudentFactory.php
+++ database/factories/StudentFactory.php
@@ -33,21 +33,21 @@ class StudentFactory extends Factory
         $ahzab_up = $faker->numberBetween(0, 30);
         $ahzab_down = $faker->numberBetween(0, 30);
         return [
-            'club_id' => Club::factory(),
+            'club_id' =>  Club::inRandomOrder()->first()->id,
             'first_name' => $faker->firstName,
             'last_name' => $faker->lastName,
             'gender' => $faker->randomElement(['male', 'female']),
             'birthdate' => $faker->dateTimeBetween('-60 years', '-3 years')->format('Y-m-d'),
             'social_status' => $faker->randomElement(['good', 'mid', 'low']),
-            'cronic_disease' => $faker->optional()->word,
+            'cronic_disease' => rand(0, 1) ? 'لا يوجد' : 'حساسية موسمية',
             'family_status' => $faker->optional()->word,
             'father_id' => $hasFather ? Guardian::factory()->state(['gender' => 'male']) : null,
             'mother_id' => $hasMother ? Guardian::factory()->state(['gender' => 'female']) : null,
-            'category_id' => Category::factory(),
+            'category_id' => Category::inRandomOrder()->first()->id,
             'ahzab_up' => $ahzab_up,
             'ahzab_down' => $ahzab_down,
             'ahzab' => $ahzab_up + $ahzab_down,
-            'subscription' => $faker->randomElement([0, 1500, 2000, 2500, 3000]),
+            'subscription' => $faker->randomElement([0,500,1000, 1500, 2000]),
             'subscription_expire_at' => $faker->optional()->dateTimeBetween('-8 months', '+8 months'),
             'insurance_expire_at' => $faker->optional()->dateTimeBetween('-3 months', '+8 months'),
         ];
