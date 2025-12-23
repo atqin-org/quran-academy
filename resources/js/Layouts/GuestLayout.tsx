e9ffcce75ccebcf926f1f93bb2686c32b190747c
ItsTarikBTW <tarikturbo007@gmail.com>
2025-12-23 13:48:45 +0100

    Refactor code structure for improved readability and maintainability

--- resources/js/Layouts/GuestLayout.tsx
+++ resources/js/Layouts/GuestLayout.tsx
@@ -1,11 +1,17 @@
-import ApplicationLogo from '@/Components/ApplicationLogo';
 import { Link } from '@inertiajs/react';
 import { PropsWithChildren } from 'react';
 
 export default function Guest({ children }: PropsWithChildren) {
     return (
         <div className="h-[100svh] flex flex-col justify-center items-center pt-6 bg-center bg-[url('/background.jpg')] bg-cover sm:px-0 px-5">
-            <div className="w-full max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden rounded-lg">
+            <div className="w-full max-w-md px-6 py-6 bg-white shadow-md overflow-hidden rounded-lg">
+                <Link href="/" className="flex justify-center mb-4">
+                    <img
+                        src="/images/athar-logo.svg"
+                        alt="Athar Logo"
+                        className="w-24 h-24"
+                    />
+                </Link>
                 {children}
             </div>
         </div>
